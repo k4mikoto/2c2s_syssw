7c04afe049b6546b93984033ae84633bb4ae155f
faseri <26807919+faseri@users.noreply.github.com>
2021-03-30 13:24:04 +0300

    lab 1

---
+++ syspo.cpp
@@ -0,0 +1,92 @@
+#include <stack>
+#include <string>
+#include <iostream>
+
+int priority(char op){
+	switch(op){
+		case '+':
+		case '-': return 1;
+		case '*':
+		case '/': return 2;
+	}
+	return 0;
+}
+
+double value(double a, double b, char op){
+	switch(op){
+		case '+': return a + b;
+		case '-': return a - b;
+		case '*': return a * b;
+		case '/': return a / b;
+	}
+	return 0;
+}
+
+double solve(std::string message){
+	int i;
+	double a,b,val;
+	char op;
+	std::stack<double> nums;
+	std::stack<char> acts;
+	for(i = 0; i < message.length(); ++i){
+		if(message[i] == ' ')
+			continue;
+		else if(message[i] == '('){
+			acts.push(message[i]);
+		}
+		else if(isdigit(message[i])){
+			val = 0;
+			while(i < message.length() && isdigit(message[i])){
+				val = (val*10) + (message[i]-'0');
+				i++;
+			}
+			nums.push(val);
+			i--;
+		}
+		else if(message[i] == ')'){
+			while(!acts.empty() && (acts.top() != '(') ){
+				b = nums.top();
+				nums.pop();
+				a = nums.top();
+				nums.pop();
+				op = acts.top();
+				acts.pop();
+				nums.push(value(a, b, op));
+			}
+			if(!acts.empty())
+				acts.pop();
+		}
+		else{
+			while(!acts.empty() && (priority(acts.top()) >= priority(message[i])) ){
+				b = nums.top();
+				nums.pop();
+				a = nums.top();
+				nums.pop();
+				op = acts.top();
+				acts.pop();
+				nums.push(value(a, b, op));
+			}
+			acts.push(message[i]);
+		}
+	}
+	while(!acts.empty()){
+		b = nums.top();
+		nums.pop();
+		a = nums.top();
+		nums.pop();
+		op = acts.top();
+		acts.pop();
+		nums.push(value(a, b, op));
+	}
+	return nums.top();
+}
+
+int main() {
+	std::cout << solve("8+9-2*4") << "\n"; 			//  9
+	std::cout << solve("(2-6)*3+(3+4)") << "\n"; 	// -5
+	std::cout << solve("5*2+1-7/3-2") << "\n";	 	//  6.666
+	std::cout << solve("((2+1+3)+6)*4") << "\n";	// 48
+	std::cout << solve("((2))-1/2") << "\n";		//  1.5
+	system("pause");
+	return 0;
+}
